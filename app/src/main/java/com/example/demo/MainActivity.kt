79d662983278e0658244ff45dc0c4a199f338d0a
Shri Ganesh Jha <realsgj@gmail.com>
2023-07-17 14:43:26 +0000

    test: text view

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -3,20 +3,44 @@ package com.example.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.demo.ui.theme.DemoTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello Android Kotlin!")
+            InitialTextField()
+            SayTheWord("Example from Android Planet!")
         }
     }
+}
+
+@Composable
+fun SayTheWord(name: String = "Android is Best") {
+    Text(
+        text = "Hello $name",
+        fontWeight = FontWeight.ExtraBold,
+        color = Color.Green,
+        fontSize = 36.sp,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun InitialTextField() {
+    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue("example", TextRange(0, 7)))
+    }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Label") },
+    )
 }
