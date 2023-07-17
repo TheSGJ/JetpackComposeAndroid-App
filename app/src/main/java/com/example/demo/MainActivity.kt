5ac217adc2b7717cf6142da0a4d5d57db61ba53d
Shri Ganesh Jha <60005430+TheSGJ@users.noreply.github.com>
2023-07-18 00:13:58 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,28 +14,27 @@ import com.example.demo.ui.theme.DemoTheme
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DemoTheme {
-                Button(
-    contentPadding = ButtonDefaults.ButtonWithIconContentPadding
-) {
-    Icon(
-        Icons.Filled.Favorite,
-        contentDescription = "Localized description",
-        modifier = Modifier.size(ButtonDefaults.IconSize)
-    )
-    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-    Text("Like")
-}
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android Kotlin!")
+                }
             }
         }
     }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        color = Color.Green
+    )
 }
