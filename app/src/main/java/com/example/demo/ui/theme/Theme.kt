e21cda2a565a5295a99f3e227c09f45d22612f2c
Shri Ganesh Jha <realsgj@gmail.com>
2023-07-21 16:07:50 +0000

    ui code

--- app/src/main/java/com/example/demo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/demo/ui/theme/Theme.kt
@@ -1,30 +1,20 @@
 package com.example.demo.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
-import androidx.compose.material3.*
 
-private val LightColorPalette = lightColorScheme(
+private val LightColorPalette = lightColors(
     primary = MainColor,
+    primaryVariant = MainColor,
     secondary = MainColor
 )
 
 @Composable
 fun DefaultTheme(content: @Composable() () -> Unit) {
     MaterialTheme(
-        colorScheme = LightColorPalette,
+        colors = LightColorPalette,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
-}
--- app/src/main/java/com/example/demo/ui/theme/Type.kt
+++ app/src/main/java/com/example/demo/ui/theme/Type.kt
@@ -1,34 +1,20 @@
 package com.example.demo.ui.theme
 
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.demo.R
 
-// Set of Material typography styles to start with
+private val Poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_italic, style = FontStyle.Italic),
+    Font(R.font.poppins_light, weight = FontWeight.Light),
+    Font(R.font.poppins_bold, weight = FontWeight.Bold),
+    Font(R.font.poppins_black, weight = FontWeight.Black),
+)
+
 val Typography = Typography(
-        bodyLarge = TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp
-        )
-        /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    defaultFontFamily = Poppins
 )
